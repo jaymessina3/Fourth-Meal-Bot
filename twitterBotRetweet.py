c9f7bd38ecf6edf04dbf6a0fb9081a962e76f323
jaymessina3 <jaymessina3@gmail.com>
2016-12-03 19:32:55 -0500

    Update twitterBotRetweet.py

--- twitterBotRetweet.py
+++ twitterBotRetweet.py
@@ -8,17 +8,27 @@ oauth_token_secret =  "It9WTfWGMSF1oSRFxcjiL8SGkiGaENch5hGAkjFz3tL3q"
 twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 
 
-search_results = twitter.search(q="#fourthmeal", count=10)
-try:
-    for tweet in search_results["statuses"]:
-        twitter.retweet(id = tweet["id_str"])
-except Exception as e:
-    print (e)
+##search_results = twitter.search(q="#fourthmeal", count=10)
+##try:
+##    for tweet in search_results["statuses"]:
+##        twitter.retweet(id = tweet["id_str"])
+##        twitter.create_favorite(id = tweet["id_str"])
+##except Exception as e:
+##    print (e)
 
 search_results = twitter.search(q="#oberlinfourthmeal", count=10)
 try:
     for tweet in search_results["statuses"]:
         twitter.retweet(id = tweet["id_str"])
+        twitter.create_favorite(id = tweet["id_str"])
+except Exception as e:
+    print (e)
+
+search_results = twitter.search(q="#oberlin", count=10)
+try:
+    for tweet in search_results["statuses"]:
+        twitter.retweet(id = tweet["id_str"])
+        twitter.create_favorite(id = tweet["id_str"])
 except Exception as e:
     print (e)
 
@@ -26,6 +36,7 @@ search_results = twitter.search(q="#dascombfourthmeal", count=10)
 try:
     for tweet in search_results["statuses"]:
         twitter.retweet(id = tweet["id_str"])
+        twitter.create_favorite(id = tweet["id_str"])
 except Exception as e:
     print (e)
 
@@ -33,5 +44,6 @@ search_results = twitter.search(q="#somanyclappingemojis", count=10)
 try:
     for tweet in search_results["statuses"]:
         twitter.retweet(id = tweet["id_str"])
+        twitter.create_favorite(id = tweet["id_str"])
 except Exception as e:
     print (e)
